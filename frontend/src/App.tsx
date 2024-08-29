c5fd5563d98756e3682854423dc6e021bed63206
ricdeo718 <juandeossa.nc@gmail.com>
2024-08-28 22:13:24 -0500

    se implementan rutas

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,35 @@
 import axios from "axios";
 import { baseUrl } from "../routes/paths";
+import {
+  BrowserRouter,
+  Link,
+  Route,
+  Routes,
+  useNavigate,
+} from "react-router-dom";
 
 export const App = () => {
   //
+
+  return (
+    <div className="App">
+      <BrowserRouter>
+        {
+          <Routes>
+            <Route path="/" element={<C1 />} />
+            <Route path="/test" element={<C2 />} />
+            <Route path="*" element={<div>404 Not Found</div>} />
+          </Routes>
+        }
+      </BrowserRouter>
+    </div>
+  );
+};
+
+const C1 = () => {
+  //
+  const nav = useNavigate();
+
   const handleFetchGet = async () => {
     //
     const url = `${baseUrl}/test`;
@@ -50,14 +77,22 @@ export const App = () => {
       console.error(error);
     }
   };
-
   return (
-    <div className="App">
+    <>
       <h1 className="text-3xl font-bold m-2">I am the frontend</h1>
-      <div className="flex flex-col gap-4">
+      <div className="flex flex-col gap-4 items-center">
         <button onClick={handleFetchGet}>Fetch Get</button>
         <button onClick={handleFetchDelete}>Fetch Delete</button>
+        <Link to="/test">Go to test</Link>
       </div>
+    </>
+  );
+};
+
+const C2 = () => {
+  return (
+    <div className="grid place-content-center h-screen text-8xl">
+      <Link to="/">Go Back</Link>
     </div>
   );
 };
