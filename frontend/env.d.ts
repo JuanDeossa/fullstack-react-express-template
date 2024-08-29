17ff508deac6a67c40e7f8f8b2b5074cd27dfd38
ricdeo718 <juandeossa.nc@gmail.com>
2024-08-28 22:25:10 -0500

    delete env.d.ts

--- frontend/env.d.ts
+++
@@ -1,8 +0,0 @@
-interface ImportMetaEnv {
-  readonly VITE_API_BASE_URL: string;
-  // Agrega aquí otras variables de entorno que estés usando.
-}
-
-interface ImportMeta {
-  readonly env: ImportMetaEnv;
-}
