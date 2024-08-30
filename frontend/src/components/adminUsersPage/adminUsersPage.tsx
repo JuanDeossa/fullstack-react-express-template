8d2a86d2efe75ebdf92374e046417f7b2383c2ef
ricdeo718 <juandeossa.nc@gmail.com>
2024-08-29 21:21:11 -0500

    ui integrations

--- frontend/src/components/adminUsersPage/adminUsersPage.tsx
+++ frontend/src/components/adminUsersPage/adminUsersPage.tsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import { UserForm, UsersList } from "../";
 import { getUsers } from "../../services";
 import { UserResponse } from "../../types/user/user.interfaces";
+import { createManyUsers, deleteAllUsers } from "../../../devtools";
 
 export const AdminUsersPage = () => {
   const [users, setUsers] = useState<UserResponse[]>([]);
@@ -21,6 +22,34 @@ export const AdminUsersPage = () => {
       <div className="flex gap-8">
         <div className="w-1/3">
           <UserForm fetchUsers={fetchUsers} />
+          <div className="flex gap-4">
+            <button
+              type="button"
+              className="border border-gray-400 rounded-md bg-gray-400 p-1 font-semibold mt-2"
+              onClick={() => {
+                createManyUsers(10, fetchUsers);
+              }}
+            >
+              Registrar 10
+            </button>
+            <button
+              type="button"
+              className="border border-gray-400 rounded-md bg-red-400 p-1 font-semibold mt-2"
+              onClick={() => {
+                deleteAllUsers(
+                  users
+                    .filter(
+                      (user) =>
+                        !["DEVELOPER", "SUPER_ADMIN"].includes(user.role)
+                    )
+                    .map((user) => user.id),
+                  fetchUsers
+                );
+              }}
+            >
+              Eliminar todos
+            </button>
+          </div>
         </div>
         <UsersList users={users} fetchUsers={fetchUsers} />
       </div>
